168fce598586d6c7e73504891614014081196bcd
iwansafr <iwansafr@gmail.com>
2019-12-16 13:18:41 +0700

    db

---
+++ application/modules/home/views/templates/aranoz/meta.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="<?php echo $link_template;?>/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo $link_template;?>/css/animate.css">
+<link rel="stylesheet" href="<?php echo $link_template;?>/css/owl.carousel.min.css">
+<link rel="stylesheet" href="<?php echo $link_template;?>/css/all.css">
+<link rel="stylesheet" href="<?php echo $link_template;?>/css/flaticon.css">
+<link rel="stylesheet" href="<?php echo $link_template;?>/css/themify-icons.css">
+<link rel="stylesheet" href="<?php echo $link_template;?>/css/magnific-popup.css">
+<link rel="stylesheet" href="<?php echo $link_template;?>/css/slick.css">
+<link rel="stylesheet" href="<?php echo $link_template;?>/css/style.css">
